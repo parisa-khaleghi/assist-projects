1b6f400779f60de7c253fa56a09d7b6fe25fad06
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-09 12:03:56 +0300

    update: add commit

--- Python/Git Commit/git-commit.py
+++ Python/Git Commit/git-commit.py
@@ -1,7 +1,10 @@
 from git import repo
 
 r = repo.Repo("/Users/parisakhaleghi/Desktop/Coding/assist-projects", search_parent_directories=True)
-
 print("=====you repository is '{}'".format(r))
 
+r.index.add("git-commit.py")
+r.index.commit('update: add commit')
 
+origin = r.remotes[0]
+origin.push()
