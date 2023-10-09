32e479538020d0fc626a665e009fca36d289debd
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-09 12:54:25 +0300

    update: check date-time

--- Python/Git Commit/git-commit.py
+++ Python/Git Commit/git-commit.py
@@ -1,10 +1,13 @@
 from git import repo
+import datetime
 
 r = repo.Repo("/Users/parisakhaleghi/Desktop/Coding/assist-projects", search_parent_directories=True)
-print("=====you repository is '{}'".format(r))
+# print("=====you repository is '{}'".format(r))
+
+print(datetime.datetime)
 
 r.index.add("/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py")
-r.index.commit('fix: change directory path')
+r.index.commit('update: check date-time')
 
 origin = r.remotes[0]
 origin.push()
