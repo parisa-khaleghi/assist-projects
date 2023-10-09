e6d569a2ad75db94152b8d995e7948d15f3fc8fe
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-09 12:42:55 +0300

    fix: change directory path

--- Python/Git Commit/git-commit.py
+++ Python/Git Commit/git-commit.py
@@ -3,8 +3,8 @@ from git import repo
 r = repo.Repo("/Users/parisakhaleghi/Desktop/Coding/assist-projects", search_parent_directories=True)
 print("=====you repository is '{}'".format(r))
 
-r.index.add("git-commit.py")
-r.index.commit('update: add commit')
+r.index.add("/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py")
+r.index.commit('fix: change directory path')
 
 origin = r.remotes[0]
 origin.push()
