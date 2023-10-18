db594906bd94c95350309869ec7c56725d461d1a
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-18 16:10:01 +0300

    update: change timezone to turkey

--- Python/Git Commit/git-commit.py
+++ Python/Git Commit/git-commit.py
@@ -2,17 +2,12 @@ from git import repo
 import datetime
 import dateutil.tz
 
+
+# def commit(message, year, month, day, hour, minute, )
 r = repo.Repo("/Users/parisakhaleghi/Desktop/Coding/assist-projects", search_parent_directories=True)
-# print("=====you repository is '{}'".format(r))
-
-# print(datetime.datetime.now()-datetime.timedelta(minutes=5))
-# print(datetime.now(datetime.timezone.utc))
-
-# print(datetime.datetime(2016, 10, 4, 12, 13, tzinfo=dateutil.tz.tzoffset(u'CEST', -7200)))
-
 r.index.add("/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py")
-r.index.commit('test: fixing error to commit for 5 min earlier', 
-               commit_date=datetime.datetime(2016, 10, 4, 12, 13, tzinfo=dateutil.tz.tzoffset(u'CEST', -7200)))
+r.index.commit('update: change timezone to turkey', 
+            commit_date=datetime.datetime(2016, 10, 4, 12, 15, tzinfo=dateutil.tz.tzoffset(u'TRT', 3*3600)), )
 
 origin = r.remotes[0]
 origin.push()
