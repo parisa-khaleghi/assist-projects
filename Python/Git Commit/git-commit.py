aae500f696556d8a6eb62baed7f073589c4b0677
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-18 16:19:52 +0300

    update: change timezone to turkey

--- Python/Git Commit/git-commit.py
+++ Python/Git Commit/git-commit.py
@@ -6,7 +6,7 @@ import dateutil.tz
 def commit(project_path, file_path, message, year, month, day, hour, minute):
     r = repo.Repo(project_path, search_parent_directories=True)
     r.index.add(file_path)
-    r.index.commit(message, commit_date=datetime.datetime(year, month, day, hour, minute, tzinfo=dateutil.tz.tzoffset(u'CEST', -7200)), )
+    r.index.commit(message, commit_date=datetime.datetime(year, month, day, hour, minute, tzinfo=dateutil.tz.tzoffset(u'TRT', 3*3600)), )
     origin = r.remotes[0]
     origin.push()
 
@@ -15,7 +15,7 @@ project_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects"
 file_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py"
 
 # fill out information for commiting
-message = 'update: put the code in a function'
+message = 'update: change timezone to turkey'
 year = datetime.datetime.now().year
 month = datetime.datetime.now().month
 day = datetime.datetime.now().day
