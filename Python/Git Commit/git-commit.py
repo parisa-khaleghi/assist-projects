547c732cbf25388d2be7e86db4e5a1fb48c2d2dd
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-18 16:01:58 +0300

    test: fixing error to commit for 5 min earlier

--- Python/Git Commit/git-commit.py
+++ Python/Git Commit/git-commit.py
@@ -1,5 +1,6 @@
 from git import repo
 import datetime
+import dateutil.tz
 
 r = repo.Repo("/Users/parisakhaleghi/Desktop/Coding/assist-projects", search_parent_directories=True)
 # print("=====you repository is '{}'".format(r))
@@ -7,8 +8,11 @@ r = repo.Repo("/Users/parisakhaleghi/Desktop/Coding/assist-projects", search_par
 # print(datetime.datetime.now()-datetime.timedelta(minutes=5))
 # print(datetime.now(datetime.timezone.utc))
 
+# print(datetime.datetime(2016, 10, 4, 12, 13, tzinfo=dateutil.tz.tzoffset(u'CEST', -7200)))
+
 r.index.add("/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py")
-r.index.commit('update: commit for 5 min earlier')
+r.index.commit('test: fixing error to commit for 5 min earlier', 
+               commit_date=datetime.datetime(2016, 10, 4, 12, 13, tzinfo=dateutil.tz.tzoffset(u'CEST', -7200)))
 
 origin = r.remotes[0]
 origin.push()
