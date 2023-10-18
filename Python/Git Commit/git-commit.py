dc24716e245050c552a1e0c19443bd8baf9db0ac
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-18 16:19:24 +0300

    update: put the code in a function

--- Python/Git Commit/git-commit.py
+++ Python/Git Commit/git-commit.py
@@ -2,12 +2,25 @@ from git import repo
 import datetime
 import dateutil.tz
 
+# function to commit in costum date
+def commit(project_path, file_path, message, year, month, day, hour, minute):
+    r = repo.Repo(project_path, search_parent_directories=True)
+    r.index.add(file_path)
+    r.index.commit(message, commit_date=datetime.datetime(year, month, day, hour, minute, tzinfo=dateutil.tz.tzoffset(u'CEST', -7200)), )
+    origin = r.remotes[0]
+    origin.push()
 
-# def commit(message, year, month, day, hour, minute, )
-r = repo.Repo("/Users/parisakhaleghi/Desktop/Coding/assist-projects", search_parent_directories=True)
-r.index.add("/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py")
-r.index.commit('update: change timezone to turkey', 
-            commit_date=datetime.datetime(2016, 10, 4, 12, 15, tzinfo=dateutil.tz.tzoffset(u'TRT', 3*3600)), )
+# fill out information for project path and file path on your system
+project_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects"
+file_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py"
 
-origin = r.remotes[0]
-origin.push()
+# fill out information for commiting
+message = 'update: put the code in a function'
+year = datetime.datetime.now().year
+month = datetime.datetime.now().month
+day = datetime.datetime.now().day
+hour = datetime.datetime.now().hour
+minute = datetime.datetime.now().minute
+
+# call the function with custom data
+commit(project_path, file_path, message, year, month, day, hour, minute)
