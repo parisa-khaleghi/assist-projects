667c0ef7f95bf421784b589cd089d9d532ad5e71
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-18 13:05:32 +0300

    update: commit for 5 min earlier

--- Python/Git Commit/git-commit.py
+++ Python/Git Commit/git-commit.py
@@ -4,10 +4,11 @@ import datetime
 r = repo.Repo("/Users/parisakhaleghi/Desktop/Coding/assist-projects", search_parent_directories=True)
 # print("=====you repository is '{}'".format(r))
 
-print(datetime.datetime.now()-datetime.timedelta(minutes=5))
+# print(datetime.datetime.now()-datetime.timedelta(minutes=5))
+# print(datetime.now(datetime.timezone.utc))
 
 r.index.add("/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py")
-r.index.commit('update: check 5 min earlier')
+r.index.commit('update: commit for 5 min earlier')
 
 origin = r.remotes[0]
 origin.push()
