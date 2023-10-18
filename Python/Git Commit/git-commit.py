bae7001f55c9eb5f020f7bf34b18b8702bdebae3
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-18 17:44:00 +0300

    test: check for 2 month ago

--- Python/Git Commit/git-commit.py
+++ Python/Git Commit/git-commit.py
@@ -15,9 +15,9 @@ project_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects"
 file_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py"
 
 # fill out information for commiting
-message = 'fix: object cannot be interpreted as an integer'
+message = 'test: check for 2 month ago'
 year = datetime.datetime.now().year
-month = datetime.datetime.now().month
+month = datetime.datetime.now().month-2
 day = datetime.datetime.now().day
 hour = datetime.datetime.now().hour
 minute = datetime.datetime.now().minute
