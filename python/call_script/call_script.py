d3eb16e6d76df2f14c40303d30dbecc9dbfb4a17
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-20 10:38:38 +0300

    init: create file to test calling script

---
+++ python/call_script/call_script.py
@@ -0,0 +1,11 @@
+#============================================#
+#            CALL SCRIPT FROM GIT            #
+#                                            #
+#                For Example                 #
+#     I will call the 'commit' function      # 
+#         from this repo located in          #
+#     "python/git_commit/git_commit.py"      #
+#============================================#
+
+
+
