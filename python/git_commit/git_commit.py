b8a4458eaa104dd8dd52fde241611c6851dd0322
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-20 09:29:59 +0300

    update: comment the sample test

--- python/git_commit/git_commit.py
+++ python/git_commit/git_commit.py
@@ -11,17 +11,17 @@ def commit(project_path, file_path, message, year, month, day, hour, minute):
     origin.push()
 
 
-# fill out information for project path and file path on your system
-project_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects"
-file_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py"
+# # fill out information for project path and file path on your system
+# project_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects"
+# file_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Git Commit/git-commit.py"
 
-# fill out information for commiting
-message = 'update: remove unnecessary lines'
-year = datetime.datetime.now().year
-month = datetime.datetime.now().month-2
-day = datetime.datetime.now().day
-hour = datetime.datetime.now().hour
-minute = datetime.datetime.now().minute
+# # fill out information for commiting
+# message = 'update: remove unnecessary lines'
+# year = datetime.datetime.now().year
+# month = datetime.datetime.now().month-2
+# day = datetime.datetime.now().day
+# hour = datetime.datetime.now().hour
+# minute = datetime.datetime.now().minute
 
-# call the function with custom data
-commit(project_path, file_path, message, year, month, day, hour, minute)
+# # call the function with custom data
+# commit(project_path, file_path, message, year, month, day, hour, minute)
