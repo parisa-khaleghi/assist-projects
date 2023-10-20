f44803511adbc3a595171a38957509933e8f43f4
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-20 23:30:27 +0300

    update: remove section for commit on gut

--- python/extract_video_url/extract_video_url.py
+++ python/extract_video_url/extract_video_url.py
@@ -1,26 +1,4 @@
-from git import repo
-import datetime
-import dateutil.tz
 from bs4 import BeautifulSoup
-
-# function to commit in costum date
-def commit(project_path, file_path, message, year, month, day, hour, minute):
-    r = repo.Repo(project_path, search_parent_directories=True)
-    r.index.add(file_path)
-    r.index.commit(message, commit_date=datetime.datetime(year, month, day, hour, minute, tzinfo=dateutil.tz.tzoffset(u'TRT', 3*3600)))
-    origin = r.remotes[0]
-    origin.push()
-
-# fill out information for project path and file path on your system
-project_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects"
-file_path = "/Users/parisakhaleghi/Desktop/Coding/assist-projects/Python/Extract video url/extract-video-url.py"
-
-# fill out information for commiting
-year = datetime.datetime.now().year
-month = datetime.datetime.now().month-2
-day = datetime.datetime.now().day
-hour = datetime.datetime.now().hour
-minute = datetime.datetime.now().minute
 
 # HTML code with embedded video
 html_code = """
@@ -46,9 +24,3 @@ video_urls = [iframe['src'] for iframe in iframes]
 # Print the extracted video URLs
 for url in video_urls:
     print(url)
-
-
-message = 'update: use BeautifulSoup to extract video links'
-
-# call the function with custom data
-commit(project_path, file_path, message, year, month, day, hour, minute)
