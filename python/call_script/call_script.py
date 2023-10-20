d01b4f72b83d43c3c63f64c486dfa56d27a05306
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-20 23:17:29 +0300

    update: import the madule and check if it work

--- python/call_script/call_script.py
+++ python/call_script/call_script.py
@@ -13,5 +13,7 @@ import sys
 # Append the path to the external repository that you cloned on your system
 sys.path.append('/Users/parisakhaleghi/Desktop/Coding/assist_projects/python/git_commit')
 
-print(sys.path)
+# Import the specific function from the script in the external repository
+from git_commit import commit
 
+print(commit())
